8a4bd741a710879358474bdc69cb80afbcfd825f
dvish2003 <vishanchathuranga63@gmail.com>
2025-12-28 18:10:14 +0530

    feat: update Jenkinsfile for improved EC2 deployment process; modify server.js to enable CORS for specific origins; update docker-compose.yaml with new MongoDB URL; enhance apiClient to use environment variable for API URL

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,15 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "http://13.51.48.151:3000",
+    "http://localhost:3000"
+  ],
+  credentials: true
+}));
 
 app.use("/api/user", userRoute);
 
@@ -19,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("Server is running........");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,12 +1,14 @@
-export const apiClient = async (endpoint, method, body = null) => {
-    const response = await fetch(`http://localhost:5000/api${endpoint}`, {
-        method: method,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: body ? JSON.stringify(body) : null,
-        cache: "no-store",
-    });
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-    return response;
-}
+export const apiClient = async (endpoint, method = "GET", body = null) => {
+  const res = await fetch(`${API_URL}/api${endpoint}`, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body ? JSON.stringify(body) : null,
+    cache: "no-store",
+  });
+
+  return res;
+};
