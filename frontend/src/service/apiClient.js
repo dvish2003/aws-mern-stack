0bfc39820cfac0a76e58222a6f95a07e313f99dd
dvish2003 <vishanchathuranga63@gmail.com>
2025-12-29 03:49:38 +0530

    feat: update apiClient.js to use API_URL for fetch requests

--- frontend/src/service/apiClient.js
+++ frontend/src/service/apiClient.js
@@ -1,7 +1,7 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const apiClient = async (endpoint, method = "GET", body = null) => {
-  const res = await fetch(`${API_URL}/api${endpoint}`, {
+  const res = await fetch(`http://13.51.48.151:5000/api${endpoint}`, {
     method,
     headers: {
       "Content-Type": "application/json",
